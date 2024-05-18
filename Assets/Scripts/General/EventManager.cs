8514cb9ef1493f06ff7c1caad2f502da2817c61d
Cliff Lee CL <tommyboys0107@gmail.com>
2024-05-18 17:54:10 +0800

    [Modify] Add chooseSkillCanvas and eventManager.

---
+++ Assets/Scripts/General/EventManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace CliffLeeCL
+{
+    /// <summary>
+    /// This singleton class manage all events in the game.
+    /// </summary>
+    /// <code>
+    /// // Usage in other class example:\n
+    /// void Start(){\n
+    ///     EventManager.instance.onGameOver += LocalFunction;\n
+    /// }\n
+    /// \n
+    /// // If OnEnable function will cause error, try listen to events in Start function.\n
+    /// void OnEnable(){\n
+    ///     EventManager.instance.onGameOver += LocalFunction;\n
+    /// }\n
+    /// \n
+    /// void OnDisable(){\n
+    ///     EventManager.instance.onGameOver -= LocalFunction;\n
+    /// }\n
+    /// \n
+    /// void LocalFunction(){\n
+    ///     //Do something here\n
+    /// }
+    /// </code>
+    public class EventManager : Singleton<EventManager>
+    {
+        /// <summary>
+        /// The event is called when game over.
+        /// </summary>
+        public event Action onGameOver;
+
+        public void OnGameOver()
+        {
+            onGameOver?.Invoke();
+            Debug.Log("OnGameOver event is invoked!");
+        }
+    }
+}
+
