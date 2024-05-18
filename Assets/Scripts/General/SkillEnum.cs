6650f6246fae11d0600ec6f3e40c43c0dea11e38
Cliff Lee CL <tommyboys0107@gmail.com>
2024-05-18 18:02:23 +0800

    [Modify] Chaser & escaper skill enum.

---
+++ Assets/Scripts/General/SkillEnum.cs
@@ -0,0 +1,18 @@
+public enum ChaserSkill
+{
+    AxeStraightThrow = 1,
+    AxeFlyFaster = 2,
+    AxeThrowFaster = 3,
+    AxeDamageUp = 4,
+    AxeBoomerang = 5,
+    FakeGhostWeapon = 6
+}
+
+public enum EscaperSkill
+{
+    Flash = 1,
+    Heal = 2,
+    SpeedUp = 3,
+    Shield = 4,
+    DoubleJump = 5,
+}
